ad3f5da037160b7f413f81e8ad272197758b36ea
MarllonCampos <marlloncampos@outlook.com>
2020-08-05 02:42:50 -0300

    router dom adicionado e end points dar aulas e estudar

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 import logoImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
+
 import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
-
+import './styles.css'
 
 function Landing() {
   return (
@@ -23,15 +25,15 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
-            <img src={giveClassesIcon} alt="Estudar" />
-            Estudar
-          </a>
+          <Link to="/give-classes" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
+          </Link>
         </div>
 
         <span className="total-connections">Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/></span>
