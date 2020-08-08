234cdb42a954200389795363bdbb5f324a48739a
MarllonCampos <marlloncampos@outlook.com>
2020-08-08 02:16:45 -0300

    Criando component input para compartilhar entre paginas

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 
 import logoImg from "../../assets/images/logo.svg";
 import backIcon from "../../assets/images/icons/back.svg";
-import './styles.css'
+import "./styles.css";
 
 interface PageHeaderProps {
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -21,6 +22,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
       <div className="header-content">
         <strong>{props.title}</strong>
+        { props.description && <p>{props.description}</p> }
         {props.children}
       </div>
     </header>
