d4b8e8c080ee81c5784961e6b52b1adb97a155c4
MarllonCampos <marlloncampos@outlook.com>
2020-08-08 06:22:48 -0300

    Trocado a forma de hardcoded que era antes para uma forma mais fluida passada por meio do props

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,42 +1,45 @@
-import React from 'react'
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
-import './styles.css'
-function TeacherItem(){
-    return(
-        <article className="teacher-item">
-          <header>
-            <img
-              src="https://avatars0.githubusercontent.com/u/52542050?s=460&u=89863cb30413d677d5d075802ead3e4871901c28&v=4://github.com/account"
-              alt="MarllonCampos"
-            />
-            <div>
-              <strong>Marllon campos</strong>
-              <span>Quimica</span>
-            </div>
-          </header>
+import React from "react";
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+import "./styles.css";
 
-          <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br /> <br /> Apaixonado por explodir coisas em laboratório e por
-            mudar a vida das pessoas através de experiências. Mais de 200.000
-            pessoas já passaram por uma das minhas explosões.
-          </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 35,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsappIcon} alt="Whatsapp"/>
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
-    )
+export interface Teacher{
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+  
 }
 
+interface TeacherItemProps {
+  classes: Teacher;
+}
+export const TeacherItem: React.FC<TeacherItemProps> = ({ classes }) => {
+  return (
+    <article className="teacher-item">
+      <header> 
+        <img src={classes.avatar} alt={classes.name} />
+        <div>
+          <strong>{classes.name}</strong>
+          <span>{classes.subject}</span>
+        </div>
+      </header>
 
+      <p>{classes.bio}</p>
 
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {classes.cost},00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
 
-export default TeacherItem;
