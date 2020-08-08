204e3a89ff3d18015343a01d4b57f10beb4fdc3b
MarllonCampos <marlloncampos@outlook.com>
2020-08-08 03:46:13 -0300

    Conectado o front-end com o back-end trazendo o total de conexoes na landing page por meio do useEffect e useState como tambem o axios

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {Link} from 'react-router-dom'
+
 import logoImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
 
@@ -7,9 +8,21 @@ import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
+import api from '../../services/api'
+
 import './styles.css'
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('/connections').then(response => {
+      const {total} = response.data
+        setTotalConnections(total)
+    })
+  },[])
+
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -36,7 +49,7 @@ function Landing() {
           </Link>
         </div>
 
-        <span className="total-connections">Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/></span>
+        <span className="total-connections">Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/></span>
       </div>
     </div>
   );
--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import PageHeader from "../../components/PageHeader";
 import Input from "../../components/Input";
 import Textarea from "../../components/Textarea";
